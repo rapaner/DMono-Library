8fbe2baeef0cceef49275a10a237514b0afb48c2
rapaner <rapaner@yandex.ru>
2025-10-14 23:22:18 +0300

    Чистка кода

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -9,15 +9,15 @@ public partial class AppShell : Shell
         try
         {
             System.Diagnostics.Debug.WriteLine("=== AppShell constructor started ===");
-            
+
             InitializeComponent();
             System.Diagnostics.Debug.WriteLine("=== AppShell InitializeComponent completed ===");
-            
+
             // Регистрация маршрутов для навигации
             Routing.RegisterRoute(nameof(YandexDiskPage), typeof(YandexDiskPage));
             Routing.RegisterRoute(nameof(ReadingSchedulePage), typeof(ReadingSchedulePage));
             System.Diagnostics.Debug.WriteLine("=== AppShell routes registered ===");
-            
+
             System.Diagnostics.Debug.WriteLine("=== AppShell constructor completed ===");
         }
         catch (Exception ex)
