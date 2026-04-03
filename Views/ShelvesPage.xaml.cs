ba79bcd3726fb50eed5f76f8da0113acf1769ac3
Bragin Dmitry <bragin.d@bookcentre.ru>
2026-04-03 11:23:51 +0300

    Исправлена загрузка полок

--- Views/ShelvesPage.xaml.cs
+++ Views/ShelvesPage.xaml.cs
@@ -4,9 +4,18 @@ namespace Library.Views;
 
 public partial class ShelvesPage : BasePage
 {
+    private readonly ShelvesViewModel _viewModel;
+
     public ShelvesPage(ShelvesViewModel viewModel)
     {
         InitializeComponent();
+        _viewModel = viewModel;
         BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        SafeExecute(async () => await _viewModel.LoadDataCommand.ExecuteAsync(null));
     }
 }
